9b0cf3369a1b10c9e856f1c63269a5b25ac6d06d
Daythyn <dprice10@stu.jsu.edu>
2023-09-08 12:00:49 -0500

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,9 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        StringBuilder rmessage = new StringBuilder(message);
+        rmessage.reverse();
+        return rmessage.toString();
     }
     
 }
